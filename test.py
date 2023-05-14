758d138220f4fd4ee9c76de148ea1b29db0ef4e8
husnafrylazz <121673134+husnafrylazz@users.noreply.github.com>
2023-05-14 21:38:42 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,41 @@
-class Buku:
-    def __init__(self, judul):
+class Member:
+    def __init__(self, kode, nama):
+        self.kode = kode
+        self.nama = nama
+
+
+class Pinjaman:
+    def __init__(self, member, judul):
+        self.member = member
         self.judul = judul
 
-    def __str__(self):
-        return self.judul
 
+class Perpustakaan:
+    def __init__(self):
+        self.pinjaman = []
 
-data_buku = []
+    def tambah_pinjaman(self, kode, nama, judul):
+        member = Member(kode, nama)
+        pinjaman = Pinjaman(member, judul)
+        self.pinjaman.append(pinjaman)
+        print("Pinjaman berhasil ditambahkan.")
 
-jumlah_data = int(input("Masukkan Jumlah Data: "))
+    def tampilkan_pinjaman(self):
+        print("Output Data Pinjaman:")
+        for i, pinjaman in enumerate(self.pinjaman, start=1):
+            print("", pinjaman.member.kode,"|",pinjaman.member.nama,"|",pinjaman.judul)
+            print()
 
-for _ in range(jumlah_data):
-    nama = input("Masukkan Nama Peminjam: ")
+perpustakaan = Perpustakaan()
+
+ulangi = "y"
+while ulangi.lower() == "y":
+    kode = input("Masukkan Kode Member: ")
+    nama = input("Masukkan Nama Member: ")
     judul = input("Masukkan Judul Buku: ")
-    buku = Buku(judul)
-    data_buku.append((nama, buku))
 
-print("Data Buku:")
-for nama, buku in data_buku:
-    print("Nama Peminjam:", nama)
-    print("Judul Buku:", buku)
-    print()
+    perpustakaan.tambah_pinjaman(kode, nama, judul)
+
+    ulangi = input("Ulangi lagi (y/n): ")
+
+perpustakaan.tampilkan_pinjaman()
