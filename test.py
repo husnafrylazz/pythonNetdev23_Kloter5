73b5f25d37286544482e1ec22f80b8ded9c5a61b
husnafrylazz <121673134+husnafrylazz@users.noreply.github.com>
2023-05-15 00:16:03 +0700

    Update test.py

--- test.py
+++ test.py
@@ -18,24 +18,31 @@ class Perpustakaan:
         member = Member(kode, nama)
         pinjaman = Pinjaman(member, judul)
         self.pinjaman.append(pinjaman)
-        print("Pinjaman berhasil ditambahkan.")
 
-    def tampilkan_pinjaman(self):
-        print("Output Data Pinjaman:")
-        for i, pinjaman in enumerate(self.pinjaman, start=1):
-            print("", pinjaman.member.kode,"|",pinjaman.member.nama,"|",pinjaman.judul)
-            print()
+    def tampilkan_pinjaman(self, kode):
+        for pinjaman in self.pinjaman:
+            if pinjaman.member.kode == kode:
+                print("Kode Member\t\t:", pinjaman.member.kode)
+                print("Nama Member\t\t:", pinjaman.member.nama)
+                print("Judul Buku\t\t:", pinjaman.judul)
+                return
+
+        print("\nData peminjaman tidak ditemukan.")
+
 
 perpustakaan = Perpustakaan()
 
 ulangi = "y"
 while ulangi.lower() == "y":
-    kode = input("Masukkan Kode Member: ")
+    kode = input("\nMasukkan Kode Member: ")
     nama = input("Masukkan Nama Member: ")
     judul = input("Masukkan Judul Buku: ")
 
     perpustakaan.tambah_pinjaman(kode, nama, judul)
-
+    perpustakaan.tampilkan_pinjaman(kode)
     ulangi = input("Ulangi lagi (y/n): ")
+    print("Ulangi lagi (y/n)\t: ", ulangi)
 
-perpustakaan.tampilkan_pinjaman()
+kode_member = input("\nMasukkan Kode Member: ")
+print("\n\nMasukkan Kode Member\t: ", kode_member)
+perpustakaan.tampilkan_pinjaman(kode_member)
