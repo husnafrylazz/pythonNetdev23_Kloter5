a8c762de59c8b428bca1c13e186a691a68a0386d
ramizass <88464165+ramizass@users.noreply.github.com>
2023-05-15 23:43:24 +0700

    Update WaktuPinjam_test1.py

--- WaktuPinjam_test1.py
+++ WaktuPinjam_test1.py
@@ -2,15 +2,12 @@ from datetime import datetime #import modul datetime
 
 def WaktuPinjam(x): 
     tgl_kembali = datetime.strptime(x, '%Y-%m-%d') #konversi string ke format YYYY-MM-DD
-
-    tgl_pinjam = datetime.today().date() #untuk current date
-
-    selisih = (tgl_kembali.date() - tgl_pinjam).days
-
+    tgl_pinjam = datetime.today().date() #untuk current date tanpa jam, menit, detik
+    selisih = (tgl_kembali.date() - tgl_pinjam).days #selisih dengan atribut .days agar jdi bil.bulat
     return selisih
 
 tgl_pinjam = datetime.today().date().strftime('%Y-%m-%d') #kebalikan strptime, konversi format datetime YYYY-MM-DD ke string
-tgl_kembali = input("Tanggal pengembalian (YYYY-MM-DD): ")
+tgl_kembali = input("Tanggal pengembalian (YYYY-MM-DD): ") #input tanggal pengembalian
 selisih = WaktuPinjam(tgl_kembali)
 
 print("Tanggal peminjaman: " + tgl_pinjam)
