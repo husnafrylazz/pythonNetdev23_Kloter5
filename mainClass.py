4d5c15bd572b4e6e1754df30e7c945050ebb71e2
ramizass <88464165+ramizass@users.noreply.github.com>
2023-05-19 23:40:04 +0700

    Add files via upload
    Revisi penggabungan fungsi WaktuPinjam() ke dalam class

--- mainClass.py
+++ mainClass.py
@@ -1,4 +1,6 @@
-class pinjamBuku:
+from datetime import datetime  # import modul datetime
+
+class mainPerpus:
     def __init__(self, kode, nama, judul_buku, tgl_pinjam): #definisikan atribut dan parameter
         self.kode = kode
         self.nama = nama
@@ -6,4 +8,9 @@ class pinjamBuku:
         self.tgl_pinjam = tgl_pinjam
         self.tgl_kembali = self.tgl_pinjam + timedelta(days=7) #timedelta agar dapat menambahkan hari ke depan
 
+    def WaktuPinjam(x):
+        tgl_kembali = datetime.strptime(x, '%y-%m-%d')  # konversi string ke format YYYY-MM-DD
+        tgl_pinjam = datetime.today().date()  # untuk current date tanpa jam, menit, detik
+        selisih = (tgl_kembali.date() - tgl_pinjam).days  # selisih dengan atribut .days agar jdi bil.bulat
+        return selisih
 
