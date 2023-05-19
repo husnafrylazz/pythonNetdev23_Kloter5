a9d54d592e0994a9a0bef1180738c090fb84dd7e
ramizass <88464165+ramizass@users.noreply.github.com>
2023-05-20 00:41:27 +0700

    Update mainClass.py

--- mainClass.py
+++ mainClass.py
@@ -8,7 +8,7 @@ class mainPerpus:
         self.tgl_pinjam = tgl_pinjam
         self.tgl_kembali = self.tgl_pinjam + timedelta(days=7) #timedelta agar dapat menambahkan hari ke depan
 
-    def WaktuPinjam(x):
+    def WaktuPinjam(self, x):
         tgl_kembali = datetime.strptime(x, '%y-%m-%d')  # konversi string ke format YYYY-MM-DD
         tgl_pinjam = datetime.today().date()  # untuk current date tanpa jam, menit, detik
         selisih = (tgl_kembali.date() - tgl_pinjam).days  # selisih dengan atribut .days agar jdi bil.bulat
